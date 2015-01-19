d079a596895de8d6887fd61627daad7d054f89f9
Peter Milne <peter.milne@d-tacq.com>
2015-01-19 19:05:22 +0000

    move LLCONTROL to sub directory for clarity 	new file:   Makefile 	new file:   afhba-llcontrol-example.c 	new file:   afhba-llcontrol-testharness.cpp

---
+++ LLCONTROL/afhba-llcontrol-example.c
@@ -0,0 +1,201 @@
+/* ------------------------------------------------------------------------- *
+ * afhba-llcontrol-example.c
+ * ------------------------------------------------------------------------- *
+ *   Copyright (C) 2014 Peter Milne, D-TACQ Solutions Ltd
+ *                      <peter dot milne at D hyphen TACQ dot com>
+ *                         www.d-tacq.com
+ *   Created on: 18 September 2014
+ *    Author: pgm
+ *                                                                           *
+ *  This program is free software; you can redistribute it and/or modify     *
+ *  it under the terms of Version 2 of the GNU General Public License        *
+ *  as published by the Free Software Foundation;                            *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with this program; if not, write to the Free Software              *
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                */
+/* ------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sched.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+
+#define HB_FILE "/dev/afhba.0.loc"
+#define HB_LEN	0x1000
+
+#define LOG_FILE	"afhba.log"
+
+void* host_buffer;
+int fd;
+int nsamples = 10000000;		/* 10s at 1MSPS */
+int sched_fifo_priority = 1;
+int verbose;
+FILE* fp_log;
+void (*G_action)(void*);
+
+/* ACQ425 */
+
+#define NCHAN	16
+#define NSHORTS	32
+#define VI_LEN 	(NSHORTS*sizeof(short))
+#define SPIX	(NCHAN*sizeof(short)/sizeof(unsigned))
+
+#if 0
+#warning MONITOR host_buffer
+#define CH01 (((volatile short*)host_buffer)[0])
+#define CH01 (((volatile short*)host_buffer)[0])
+#define CH02 (((volatile short*)host_buffer)[1])
+#define CH03 (((volatile short*)host_buffer)[2])
+#define CH04 (((volatile short*)host_buffer)[3])
+#define TLATCH (((volatile unsigned*)host_buffer)[SPIX])	/* actually, sample counter */
+#define SPAD1	(((volatile unsigned*)host_buffer)[SPIX+1])   /* user signal from ACQ */
+#else
+#define CH02 (((volatile short*)local_buffer)[1])
+#define CH03 (((volatile short*)local_buffer)[2])
+#define CH04 (((volatile short*)local_buffer)[3])
+#define TLATCH (((volatile unsigned*)local_buffer)[SPIX])	/* actually, sample counter */
+#define SPAD1	(((volatile unsigned*)local_buffer)[SPIX+1])   /* user signal from ACQ */
+#endif
+
+
+void get_mapping() {
+	fd = open(HB_FILE, O_RDWR);
+	if (fd < 0){
+		perror("HB_FILE");
+		exit(errno);
+	}
+	host_buffer = mmap(0, HB_LEN, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if (host_buffer == (caddr_t)-1 ){
+		perror( "mmap" );
+	        exit(errno);
+	}
+}
+
+void goRealTime(void)
+{
+	struct sched_param p = {};
+	p.sched_priority = sched_fifo_priority;
+
+
+
+	int rc = sched_setscheduler(0, SCHED_FIFO, &p);
+
+	if (rc){
+		perror("failed to set RT priority");
+	}
+}
+
+void write_action(void *data)
+{
+	fwrite(data, sizeof(short), NSHORTS, fp_log);
+}
+
+void check_tlatch_action(void *local_buffer)
+{
+	static unsigned tl0;
+
+	unsigned tl1 = TLATCH;
+	if (tl1 != tl0+1){
+		printf("%d => %d\n", tl0, tl1);
+	}
+	tl0 = tl1;
+}
+
+
+void ui(int argc, char* argv[])
+{
+        if (getenv("RTPRIO")){
+		sched_fifo_priority = atoi(getenv("RTPRIO"));
+        }
+	if (getenv("VERBOSE")){
+		verbose = atoi(getenv("VERBOSE"));
+	}
+	if (argc > 1){
+		nsamples = atoi(argv[1]);
+	}
+	G_action = write_action;
+	if (getenv("ACTION")){
+		if (strcmp(getenv("ACTION"), "check_tlatch") == 0){
+			G_action = check_tlatch_action;
+		}
+	}
+}
+
+void setup()
+{
+	get_mapping();
+	goRealTime();
+	fp_log = fopen("llcontrol.log", "w");
+	if (fp_log == 0){
+		perror("llcontrol.log");
+		exit(1);
+	}
+}
+
+void run(void (*action)(void*))
+{
+	short* local_buffer = calloc(NSHORTS, sizeof(short));
+	unsigned tl0 = 0xdeadbeef;	/* always run one dummy loop */
+	unsigned spad1_0 = SPAD1;
+	unsigned spad1_1;
+	unsigned tl1;
+	int sample;
+	int println = 0;
+
+	mlockall(MCL_CURRENT);
+	memset(host_buffer, 0, VI_LEN);
+
+	for (sample = 0; sample <= nsamples; ++sample, tl0 = tl1){
+		memcpy(local_buffer, host_buffer, VI_LEN);
+		while((tl1 = TLATCH) == tl0){
+			sched_yield();
+			memcpy(local_buffer, host_buffer, VI_LEN);
+		}
+		if (verbose){
+			if (sample%10000 == 0){
+				if (println == 0){
+					printf("[%10u] ", sample);
+					println = 1;
+				}
+				printf("%10u ", tl1);
+			}
+			if (spad1_1 != spad1_0){
+				if (println == 0){
+					printf("[%d] ", sample);
+					println = 1;
+				}
+				printf("\t%u => %u ", sample, spad1_0, spad1_1);
+				spad1_0 = spad1_1;
+			}
+			if (println){
+				printf("\n");
+				println = 0;
+			}
+		}
+		action(local_buffer);
+	}
+}
+
+close() {
+	munmap(host_buffer, HB_LEN);
+	close(fd);
+	fclose(fp_log);
+}
+int main(int argc, char* argv[])
+{
+	ui(argc, argv);
+	setup();
+	printf("ready for data\n");
+	run(G_action);
+	printf("finished\n");
+}
---
+++ LLCONTROL/afhba-llcontrol-testharness.cpp
@@ -0,0 +1,294 @@
+/* ------------------------------------------------------------------------- *
+ * afhba-llcontrol-example.c
+ * ------------------------------------------------------------------------- *
+ *   Copyright (C) 2014 Peter Milne, D-TACQ Solutions Ltd
+ *                      <peter dot milne at D hyphen TACQ dot com>
+ *                         www.d-tacq.com
+ *   Created on: 18 September 2014
+ *    Author: pgm
+ *                                                                           *
+ *  This program is free software; you can redistribute it and/or modify     *
+ *  it under the terms of Version 2 of the GNU General Public License        *
+ *  as published by the Free Software Foundation;                            *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with this program; if not, write to the Free Software              *
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                */
+/* ------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sched.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define HB_FILE "/dev/afhba.0.loc"
+#define HB_LEN	0x1000
+
+#define LOG_FILE	"afhba.log"
+
+void* host_buffer;
+int fd;
+int nsamples = 10000000;		/* 10s at 1MSPS */
+int sched_fifo_priority = 1;
+int verbose;
+FILE* fp_log;
+const char* action_name = "WriteAction";
+
+/* ACQ425 */
+
+#define NCHAN	16
+#define NSHORTS	32
+#define VI_LEN 	(NSHORTS*sizeof(short))
+#define SPIX	(NCHAN*sizeof(short)/sizeof(unsigned))
+
+#if 0
+#warning MONITOR host_buffer
+#define CH01 (((volatile short*)host_buffer)[0])
+#define CH01 (((volatile short*)host_buffer)[0])
+#define CH02 (((volatile short*)host_buffer)[1])
+#define CH03 (((volatile short*)host_buffer)[2])
+#define CH04 (((volatile short*)host_buffer)[3])
+#define TLATCH (((volatile unsigned*)host_buffer)[SPIX])	/* actually, sample counter */
+#define SPAD1	(((volatile unsigned*)host_buffer)[SPIX+1])   /* user signal from ACQ */
+#else
+#define CH02 (((volatile short*)local_buffer)[1])
+#define CH03 (((volatile short*)local_buffer)[2])
+#define CH04 (((volatile short*)local_buffer)[3])
+#define TLATCH (((volatile unsigned*)local_buffer)[SPIX])	/* actually, sample counter */
+#define SPAD1	(((volatile unsigned*)local_buffer)[SPIX+1])   /* user signal from ACQ */
+#endif
+
+class Action {
+public:
+	Action() {}
+	virtual void onSample(void* sample) = 0;
+	virtual ~Action() {}
+};
+
+void get_mapping() {
+	fd = open(HB_FILE, O_RDWR);
+	if (fd < 0){
+		perror("HB_FILE");
+		exit(errno);
+	}
+	host_buffer = mmap(0, HB_LEN, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if (host_buffer == (caddr_t)-1 ){
+		perror( "mmap" );
+	        exit(errno);
+	}
+}
+
+void goRealTime(void)
+{
+	struct sched_param p = {};
+	p.sched_priority = sched_fifo_priority;
+
+
+
+	int rc = sched_setscheduler(0, SCHED_FIFO, &p);
+
+	if (rc){
+		perror("failed to set RT priority");
+	}
+}
+
+void write_action(void *data)
+{
+	fwrite(data, sizeof(short), NSHORTS, fp_log);
+}
+
+
+void setAffinity(unsigned cpu_mask)
+{
+	cpu_set_t cpu_set;
+	CPU_ZERO(&cpu_set);
+	for (int cpu = 0; cpu < 32; ++cpu){
+		if ((1<<cpu) &cpu_mask){
+			CPU_SET(cpu, &cpu_set);
+		}
+	}
+	printf("setAffinity: %d,%d,%d,%d\n",
+			CPU_ISSET(0, &cpu_set), CPU_ISSET(1, &cpu_set),
+			CPU_ISSET(2, &cpu_set), CPU_ISSET(3, &cpu_set)
+			);
+
+	int rc = sched_setaffinity(0,  sizeof(cpu_set_t), &cpu_set);
+	if (rc != 0){
+		perror("sched_set_affinity");
+		exit(1);
+	}
+}
+
+void ui(int argc, char* argv[])
+{
+        if (getenv("RTPRIO")){
+		sched_fifo_priority = atoi(getenv("RTPRIO"));
+        }
+	if (getenv("VERBOSE")){
+		verbose = atoi(getenv("VERBOSE"));
+	}
+	if (argc > 1){
+		nsamples = atoi(argv[1]);
+	}
+	if (getenv("ACTION")){
+		action_name = getenv("ACTION");
+	}
+	if (getenv("AFFINITY")){
+		setAffinity(strtol(getenv("AFFINITY"), 0, 0));
+	}
+}
+
+void setup()
+{
+	get_mapping();
+	goRealTime();
+	fp_log = fopen("llcontrol.log", "w");
+	if (fp_log == 0){
+		perror("llcontrol.log");
+		exit(1);
+	}
+}
+
+void run(Action* action)
+{
+	short* local_buffer = new short[NSHORTS];
+	unsigned tl0 = 0xdeadbeef;	/* always run one dummy loop */
+	unsigned spad1_0 = SPAD1;
+	unsigned spad1_1;
+	unsigned tl1;
+	int sample;
+	int println = 0;
+
+	mlockall(MCL_CURRENT);
+	memset(host_buffer, 0, VI_LEN);
+
+	for (sample = 0; sample <= nsamples; ++sample, tl0 = tl1){
+		memcpy(local_buffer, host_buffer, VI_LEN);
+		while((tl1 = TLATCH) == tl0){
+			sched_yield();
+			memcpy(local_buffer, host_buffer, VI_LEN);
+		}
+		if (verbose){
+			if (sample%10000 == 0){
+				if (println == 0){
+					printf("[%10u] ", sample);
+					println = 1;
+				}
+				printf("%10u\n", tl1);
+			}
+		}
+		if (sample) action->onSample(local_buffer);
+	}
+}
+
+void closedown() {
+	munmap(host_buffer, HB_LEN);
+	close(fd);
+	fclose(fp_log);
+}
+
+class CheckTlatchAction: public Action {
+	unsigned tl0;
+	int tstep;
+	int nsamples;
+	int nerrors;
+	unsigned *buffer;
+public:
+	CheckTlatchAction() : tl0(0), tstep(1), nsamples(0), nerrors(0)
+	{
+		if (getenv("TSTEP")){
+			tstep = atoi(getenv("TSTEP"));
+			printf("tstep set %d\n", tstep);
+		}
+		buffer = new unsigned[::nsamples];
+	}
+        virtual void onSample(void* local_buffer) {
+		unsigned tl1 = TLATCH;
+		if (tl1 != tl0+tstep){
+			if (verbose && (verbose>1 || nerrors<5 || nerrors%1000 == 0)){
+				printf("%10d/%-10d : %d => %d\n", nerrors, nsamples, tl0, tl1);
+			}
+			++nerrors;
+		}
+		tl0 = tl1;
+		buffer[nsamples] = tl1;
+		++nsamples;
+	}
+        virtual ~CheckTlatchAction() {
+		printf("CheckTlatchAction: %d errors found in %d samples\n", nerrors, nsamples);
+		FILE *fp = fopen("tlatch.log", "w");
+		if (fp == 0) {
+			perror("tlatch.log");
+		}else{
+			fwrite(buffer, sizeof(unsigned), nsamples, fp);
+			fclose(fp);
+		}
+	}
+};
+
+
+class WriteAction: public Action {
+	char* buffer;
+	int cursor;
+public:
+	WriteAction(): cursor(0) {
+		buffer = new char[::nsamples*VI_LEN];
+	}
+        virtual void onSample(void* sample) {
+        	memcpy(buffer+cursor, sample, VI_LEN);
+        	cursor += VI_LEN;
+        }
+        virtual ~WriteAction() {
+        	Action * checkAction = new CheckTlatchAction;
+        	for (int ic2 = 0; ic2 < cursor; ic2 += VI_LEN){
+        		checkAction->onSample(buffer+ic2);
+        		fwrite(buffer+ic2, 1, VI_LEN, fp_log);
+        	}
+        	delete checkAction;
+        	printf("wrote %d bytes\n", ::nsamples*VI_LEN);
+        }
+};
+
+
+
+class NullAction: public Action {
+	int nsamples;
+public:
+	NullAction() : nsamples(0) {}
+        virtual void onSample(void* sample) {
+
+	};
+        virtual ~NullAction() {
+		printf("NullAction: nsamples:%d\n", nsamples);
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	ui(argc, argv);
+
+	Action *action;
+	if (strcmp(action_name, "WriteAction") == 0){
+		action = new WriteAction;
+	}else if (strcmp(action_name, "CheckTlatchAction") == 0){
+		action = new CheckTlatchAction;
+	}else{
+		action = new NullAction;
+	}
+	setup();
+	printf("ready for data\n");
+	run(action);
+	printf("finished\n");
+	delete action;
+	closedown();
+}
