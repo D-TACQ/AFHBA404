f271f73e0c90321726ab0550ed48f476bdb1e228
Peter Milne <peter.milne@d-tacq.com>
2015-02-04 18:10:45 +0000

    standard functional tests 	new file:   FUNCTIONAL_TESTS/Makefile 	new file:   FUNCTIONAL_TESTS/checkramp.c 	new file:   FUNCTIONAL_TESTS/hbfarm425.c

---
+++ FUNCTIONAL_TESTS/checkramp.c
@@ -0,0 +1,73 @@
+/* ------------------------------------------------------------------------- */
+/* file checkramp.c							     */
+/* ------------------------------------------------------------------------- */
+/*   Copyright (C) 2011 Peter Milne, D-TACQ Solutions Ltd
+ *                      <Peter dot Milne at D hyphen TACQ dot com>
+
+    http://www.d-tacq.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of Version 2 of the GNU General Public License
+    as published by the Free Software Foundation;
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                */
+/* ------------------------------------------------------------------------- */
+
+/** @file checkramp.c : validates ACQ196 simulate data ramp
+ * Refs: 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+
+int main(int argc, char* argv[])
+{
+	unsigned xx;
+	unsigned xx0;
+	unsigned xx1;
+	unsigned long long ii = 0;
+	unsigned errors = 0;
+	unsigned error_report = 0;
+	int verbose = 0;
+
+	if (argc > 1 && strcmp(argv[1], "-v") == 0){
+		verbose = 1;
+	}
+	for(ii = 0; fread(&xx, sizeof(xx), 1, stdin); ++ii){
+		if (ii == 0){
+			xx0 = xx1 = xx;
+		}else{
+			if (xx != xx1 + 1){
+				if (++error_report < 10){
+					printf("%012llx 0x%08x 0x%08x ** ERROR **\n",
+					       ii, xx1, xx);
+				}
+				++errors;
+			}else{
+				error_report = 0;
+			}
+			xx1 = xx;
+		}
+
+		if (verbose && ii % 0x40000 == 0){
+			printf("%012llx  %08x %08x  %d errors\n",
+			       ii, xx0, xx, errors);
+		}
+	}	
+
+	printf("%012llx bytes %d Mbytes %d errors\n",
+		ii*4, ii*4/0x100000, errors);		
+}
---
+++ FUNCTIONAL_TESTS/hbfarm425.c
@@ -0,0 +1,78 @@
+/* hbfarm425 - split off 2,3,4 x 16 channel blocks */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+FILE *fp_out[5];	/* index from 1 */
+
+const char* process;
+const char* outroot = ".";
+
+int next(int ii){
+	if (++ii > 4) ii = 1;
+	if (fp_out[ii] = 0){
+		return next(ii);		/* recursion : woohoo! */
+	}
+	return ii;
+}
+
+int hbfarm(void)
+{
+	unsigned lw[8];
+	int ii = 0;
+	while(fread(lw, sizeof(unsigned), 8, stdin) == 8){
+		fwrite(lw, sizeof(unsigned), 8, fp_out[ii]);
+		ii = next(ii);	
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int sinks = 0;
+	int ii;
+	if (argc < 2){
+		fprintf(stderr, "hbfarm425 site1 site2 site3 site4\n");	
+		return 1;
+	}	
+	if (getenv("HBFARM_PROCESS")){
+		process = getenv("HBFARM_PROCESS");		
+	}
+	if (getenv("OUTROOT")){
+		outroot = getenv("OUTROOT");
+	}
+
+	for (ii = 1; ii < argc; ++ii){
+		int site = atoi(argv[ii]);
+		if (site < 1 || site > 4){
+			fprintf(stderr, "site %d out of range\n", site);
+			return -1;
+		}else if (fp_out[site]){
+			fprintf(stderr, "site %d already in use\n", site);
+		}else{
+			if (process){
+				fp_out[site] = popen(process, "w");
+				if (fp_out[site] == 0){
+					perror(process);
+					return 1;
+				}
+			}else{
+				char fname[128];
+				sprintf(fname, "%s/hbfarm425.%d", outroot, site);
+				fp_out[site] = fopen(fname, "w");
+				if (fp_out[site] == 0){
+					perror(fname);
+					return 1;
+				}
+			}
+			sinks++;
+		}
+	}
+	if (sinks == 0){
+		fprintf(stderr, "must specify 1 or more sinks\n");
+		return 1;
+	}
+	return hbfarm();
+}
+
+
