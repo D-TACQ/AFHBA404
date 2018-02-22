f8993f26f558a6b929ba01bb4bf2ccf23ae48171
Peter Milne <peter.milne@d-tacq.com>
2018-02-22 13:25:04 +0000

    complete replication of ../scripts/llc-test-harness-extclk-2box-AI123456-AO12

--- HAPI/llc-run-full-auto-two.py
+++ HAPI/llc-run-full-auto-two.py
@@ -9,6 +9,13 @@ acq2106_llc-run-full-auto-two.py UUT1 UUT2
 import argparse
 import acq400_hapi
 import time
+import os
+
+
+EXTCLKDIV = int(os.getenv("EXTCLKDIV", "10"))
+SIMULATE = os.getenv("SIMULATE", "5")
+AISITES = os.getenv("AISITES", "1,2,3,4,5,6")
+AOSITES = os.getenv("AOSITES", "1,2")
 
 def hit_resets(svc):    
     for knob in svc.help():
@@ -20,14 +27,34 @@ def clear_counters(uuts):
         for cx in [ 'cA', 'cB']:
             hit_resets(uut.svc[cx])
             
+def init_common(uut):
+    uut.s1.clkdiv = EXTCLKDIV
+    
+def init_ai(uut):
+    init_common(uut)
+    
+    for s in uut.modules:
+        uut.modules[s].simulate = '1' if str(s) in SIMULATE else '0'
+    uut.s0.spad = '1,16,0'
+    uut.cA.spad = '1'
+    uut.cA.aggregator = 'sites={}'.format(AISITES)
+
+def init_ao(uut):
+    init_common(uut)
+    uut.s1.lotide = 256
+    uut.s0.distributor = "sites={} comms=2 pad=0 on".format(AOSITES)
+    
         
 def run_main(args):
     uuts = [ acq400_hapi.Acq2106(addr) for addr in args.uuts ]
     uut1 = uuts[0]
     uut2 = uuts[1]
     
-    clear_counters(uuts)
-
+    clear_counters(uuts)    
+    init_ai(uut1)
+    init_ao(uut2)
+    
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="aq2106_llc-run-full-auto-two.py")
     parser.add_argument("uuts", nargs=2, help="name the uuts")
