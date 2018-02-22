9b44ddd957afaa6a9db00253a5543403a05bcaf0
Peter Milne <peter.milne@d-tacq.com>
2018-02-22 12:53:16 +0000

    clears MGT counters 	modified:   llc-run-full-auto-two.py

--- HAPI/llc-run-full-auto-two.py
+++ HAPI/llc-run-full-auto-two.py
@@ -10,10 +10,23 @@ import argparse
 import acq400_hapi
 import time
 
+def hit_resets(svc):    
+    for knob in svc.help():
+        if (knob.endswith('RESET')):
+            svc.set_knob(knob, '1')            
+            
+def clear_counters(uuts):
+    for uut in uuts:
+        for cx in [ 'cA', 'cB']:
+            hit_resets(uut.svc[cx])
+            
+        
 def run_main(args):
-    uuts = [ acq400_hapi.Acq2106(addr) for addr in parser.uuts ]
+    uuts = [ acq400_hapi.Acq2106(addr) for addr in args.uuts ]
     uut1 = uuts[0]
     uut2 = uuts[1]
+    
+    clear_counters(uuts)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="aq2106_llc-run-full-auto-two.py")
