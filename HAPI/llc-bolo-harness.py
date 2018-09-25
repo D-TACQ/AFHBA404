ffaa23ea143ad691786a5a8237c4edddd3ad4411
Unknown <sean.alsop@d-tacq.co.uk>
2018-09-25 15:41:30 +0100

    Fixed init_ai function (previous commit not working).

--- HAPI/llc-bolo-harness.py
+++ HAPI/llc-bolo-harness.py
@@ -13,6 +13,7 @@ import os
 
 
 AISITES = os.getenv("AISITES", "1")
+SPAD_LEN = os.getenv("SPAD_LEN", 8)
 
 def hit_resets(svc):    
     for knob in svc.help():
@@ -26,10 +27,19 @@ def clear_counters(uuts):
             
     
 def init_ai(uut):
-    uut.cA.aggregator = 'sites={}'.format(AISITES)    
-    uut.cB.aggregator = 'sites={}'.format(AISITES)    
-
-        
+    bolo_chan_factor = 24
+    spad_len = int(SPAD_LEN)
+    uut.s0.spad = '1,{},0'.format(spad_len)
+    uut.cA.spad = '1'
+    uut.cA.aggregator = 'sites={}'.format(AISITES)
+    uut.cB.spad = '1'
+    uut.cB.aggregator = 'sites={}'.format(AISITES)
+    uut.s0.run0 = '{}'.format(AISITES)
+    # we need to calculate a new nchan based on the number of bolo sites in
+    # the uut. uut assumes bolo has 8 channels, but post dsp each site has 
+    # 24 channels. 
+    uut.s0.NCHAN = int(AISITES.split(",")[-1])*bolo_chan_factor+spad_len
+    
 def run_main(args):
     uuts = [ acq400_hapi.Acq2106(addr) for addr in args.uuts ]
    
