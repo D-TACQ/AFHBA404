a2e06c7a46dc0a44f8650a33da71fc6935ed15a0
Peter Milne <peter.milne@d-tacq.com>
2018-12-13 10:42:21 +0000

    implement the set() method 	modified:   pwm_set_channel.c

--- LLCONTROL/pwm_set_channel.c
+++ LLCONTROL/pwm_set_channel.c
@@ -28,12 +28,32 @@ struct PWM_CTRL {
 	unsigned PWM_IC:10;
 };
 
+#define MAX_IS	1
+#define MAX_GP  0x7FF
+#define MAX_OC  0x3FF
+#define MAX_IC	0x3FF
 
 int devnum = 0;
 int ibuf = 0;
 
 unsigned *pbuf;
 
+
+void _set(int chan, struct PWM_CTRL pwm)
+{
+	memcpy(&pbuf[ICHAN(chan)], &pwm, sizeof(unsigned));
+}
+
+void set(int chan, struct PWM_CTRL pwm){
+	if (chan == CHAN_ALL){
+		int cc;
+		for (cc = 1; cc <= MAXCHAN; ++cc){
+			_set(cc, pwm);
+		}
+	}else{
+		_set(chan, pwm);
+	}
+}
 void _query(int chan)
 {
 	struct PWM_CTRL pwm;
@@ -56,6 +76,15 @@ void query(int chan)
 	printf("\n");
 }
 
+unsigned limit(unsigned xx, unsigned _max)
+{
+	return xx > _max? xx: _max;
+}
+
+unsigned alimit(const char *xxs, unsigned _max)
+{
+	return limit(strtoul(xxs, 0, 0), _max);
+}
 
 int main(int argc, char* argv[])
 {
@@ -68,9 +97,19 @@ int main(int argc, char* argv[])
 	fprintf(stderr, "pwm_set_channel chan is group icount ocount ");
 	get_shared_mapping(devnum, ibuf, 0, (void**)&pbuf);
 
-	if (argc > 1) chan = atoi(argv[1]);
+	if (argc > 1) chan = alimit(argv[1], MAXCHAN);
 	if (argc > 2){
-		pwm_ctrl.PWM_IS = strtoul(argv[2], 0, 0);
+		pwm_ctrl.PWM_IS = alimit(argv[2], MAX_IS);
+		if (argc > 3){
+			pwm_ctrl.PWM_GP = alimit(argv[3], MAX_GP);
+			if (argc > 4){
+				pwm_ctrl.PWM_IC = alimit(argv[4], MAX_IC);
+				if (argc > 5){
+					pwm_ctrl.PWM_OC = alimit(argv[5], MAX_IC);
+				}
+			}
+		}
+		set(chan, pwm_ctrl);
 	}else{
 		query(chan);
 	}
