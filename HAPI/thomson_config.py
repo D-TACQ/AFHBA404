57047946e0a7ba7b3100a58a8fdeaeb190c595b5
dt100 <dt100@hoth.d-tacq>
2019-03-01 15:05:05 +0000

    basic thomson config  Committer: dt100 <dt100@hoth.d-tacq> 	modified:   thomson_config.py

--- HAPI/thomson_config.py
+++ HAPI/thomson_config.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 """
-thomson_config.py UUT1 UUT2 UUT3 UUT4  
-UUT1 is MASTER
+acq2106_llc-run-full-auto-two.py UUT1 UUT2
 """
 
 
@@ -14,6 +13,8 @@ import os
 EXTCLKDIV = int(os.getenv("EXTCLKDIV", "100"))
 SIMULATE = os.getenv("SIMULATE", "")
 AISITES = os.getenv("AISITES", "1,2,3,4,5,6")
+AOSITES = os.getenv("AOSITES", "1,2")
+DOSITES = os.getenv("DOSITES", "5")
 XOCOMMS = os.getenv("XOCOMMS", "A")
 
 
@@ -29,48 +30,22 @@ def clear_counters(uuts):
             hit_resets(uut.svc[cx])
 
 
-def init_clks(uuts):
-    print("init_clks")
-    role = "master"
+def init_clks(uut):
+    uut.s1.CLKDIV = EXTCLKDIV
+    uut.s1.clkdiv = EXTCLKDIV
+    uut.s0.SIG_SYNC_OUT_CLK_DX = "d2"
 
-    for uut in uuts:
-        uut.s0.SIG_SRC_TRG_0 = 'EXT' if role=="master" else 'HDMI'
-        print("shot trigger role:{} value:{}".format(role, uut.s0.SIG_SRC_TRG_0))
-        uut.s0.SIG_SYNC_OUT_TRG = 'TRG'
-        uut.s0.SIG_SYNC_OUT_TRG_DX = 'd0'
-        uut.s0.spad1_us = '1,0,0'
 
-        if role=="master":
-            uut.s0.SIG_SRC_TRG_1 = 'FP_SYNC'
-
-        uut.s0.SIG_SRC_CLK_0 = 'GPG0' if role=="master" else 'HDMI'
-        print("adc clk_src role:{} value:{}".format(role, uut.s0.SIG_SRC_CLK_0))
-
-        uut.s0.SIG_SYNC_OUT_CLK = 'CLK'
-        uut.s0.SIG_SYNC_OUT_CLK_DX = 'd0'
-
-        role = "slave"
-
-#    uut.s1.CLKDIV = EXTCLKDIV
-#    uut.s1.clkdiv = EXTCLKDIV
-#    uut.s0.SIG_SYNC_OUT_CLK_DX = "d2"
-
-GPGDX = 1               # output d0
-FLATTOP = 5             # pulse width
-
-def set_delay(uut, args):
-    uut.s0.GPG_ENABLE = '0'
-    stl = ""
-    stl = stl + "+{},{}\n".format(args.delay,GPGDX)
-    stl = stl + "+{},{}\n".format(FLATTOP,0)
-
-    uut.load_gpg(stl)
-    uut.s0.gpg_trg = '1,1,0'
-    uut.s0.GPG_MODE = 'LOOPWAIT'
-    uut.s0.GPG_ENABLE = '1'
+def init_spad_us(uut):
+    trg = uut.s1.trg
+    trg = trg[4:9]
+    print "trg = ", trg
+    uut.s0.spad1_us = trg
 
 
 def init_ai(uut):
+    #init_common(uut)
+    init_spad_us(uut)
     for s in uut.modules:
         uut.modules[s].simulate = '1' if str(s) in SIMULATE else '0'
     uut.s0.spad = '1,16,0'
@@ -78,27 +53,22 @@ def init_ai(uut):
     uut.cA.aggregator = 'sites={}'.format(AISITES)
     uut.cB.spad = '1'
     uut.cB.aggregator = 'sites={}'.format(AISITES)
-    uut.s1.trg = '0,0,0'
-    uut.s1.clk = '1,0,1'
 
 
 def run_main(args):
     uuts = [ acq400_hapi.Acq2106(addr) for addr in args.uuts ]
-    uutm = uuts[0]
 
     print("initialise {} uuts {}".format(len(uuts), args.uuts))
     clear_counters(uuts)
-    init_clks(uuts)
-    set_delay(uutm, args)
-    for uut in uuts:
+    init_clks(uuts[0])
+    
+    for uut in uuts[0:]:
         init_ai(uut)
 
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="thomson_config.py")
-    parser.add_argument("--delay", type=int, default=5, help="delay in usecs (min 5)")
-    parser.add_argument("--verbose", type=int, default=0, help="verbosity, default 0")
+    parser = argparse.ArgumentParser(description="aq2106_llc-run-full-auto-two.py")
     parser.add_argument("uuts", nargs='+', help="name the uuts")
     run_main(parser.parse_args())
 
