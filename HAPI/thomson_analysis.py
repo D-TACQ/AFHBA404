3c779bb2a31d0fcebb491da3c7c4a57eca3faaae
dt100 <dt100@hoth.d-tacq>
2019-03-02 13:49:43 +0000

    aicols=1,2,3 : plot CH01, CH02 CH03 etc  Committer: dt100 <dt100@hoth.d-tacq> 	modified:   thomson_analysis.py

--- HAPI/thomson_analysis.py
+++ HAPI/thomson_analysis.py
@@ -18,16 +18,24 @@ def plot_chan(data, chan, total_systems):
     plt.show()
 
 
-def run_analysis(args):
+def fix_args(args):
     args.longs = args.AI/2 + args.SP
     args.shorts = args.longs*2
     args.SPIX = args.AI/2
+    args.ai_plot = [ (c-1) for c in tuple(map(int, args.aicols.split(",")))]
+    return args
 
+
+def run_analysis(args):
     short_data = np.fromfile(args.src, dtype=np.int16)
     long_data = np.fromfile(args.src, dtype=np.int32)
 
     sample_count = check_column(long_data, args.SPIX+0, args.longs, args.nuuts)
     usec_count = check_column(long_data,   args.SPIX+1, args.longs, args.nuuts)
+
+    aicols = []
+    for col in args.ai_plot:
+        aicols.append(check_column(short_data, col, args.shorts, args.nuuts))
 
     FF = "{0:10}"					# Field Format
     UUTS = range(0, args.nuuts)
@@ -42,6 +50,10 @@ def run_analysis(args):
             rc.append(FF.format(usec_count[uut][num]))
         for uut in UUTS:
             rc.append(FF.format(0 if num <= 2 else usec_count[uut][num]-usec_count[uut][num-1]))
+
+        for uut in UUTS:
+	    for col, ch in enumerate(args.ai_plot):
+                rc.append(FF.format(aicols[col][uut][num]))
 
 	print(",".join(rc))
 
@@ -62,8 +74,9 @@ def run_main():
     parser.add_argument('--AI', type=int, default=192, help="number of AI channels (int16)")
     parser.add_argument('--SP', type=int, default=16, help="number of columns in scratchpad (int32)")
     parser.add_argument('--src', default="../LLCONTROL/afhba.0.log", help="source log file")
+    parser.add_argument('--aicols', default="", help="list of AI channels to dump index from 1")
     parser.add_argument('--verbose', type=int, default=0, help="verbose")
-    run_analysis(parser.parse_args())
+    run_analysis(fix_args(parser.parse_args()))
 
 
 if __name__  == '__main__':
