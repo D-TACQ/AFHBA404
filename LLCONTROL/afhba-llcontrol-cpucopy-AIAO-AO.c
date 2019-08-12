7907e04d206bf5e4dd2dd246ee8051941c06d786
Peter Milne <peter.milne@d-tacq.com>
2019-08-12 17:13:34 +0100

    remove merge fails 	modified:   afhba-llcontrol-cpucopy-AIAO-AO.c

--- LLCONTROL/afhba-llcontrol-cpucopy-AIAO-AO.c
+++ LLCONTROL/afhba-llcontrol-cpucopy-AIAO-AO.c
@@ -187,11 +187,7 @@ void ui(int argc, char* argv[])
 		fprintf(stderr, "SPADLONGS set %d\n", spadlongs);
 	}
 	xllc_def.len = VI_LEN;
-<<<<<<< HEAD
-    ao_only_xllc_def.len = VO_ONLY_LEN;
-=======
 	ao_only_xllc_def.len = VO_ONLY_LEN;
->>>>>>> 5a95de85c4eeb21c4a2f4b7796d9ea5d23f4b382
 
 	if (argc > 1){
 		nsamples = atoi(argv[1]);
@@ -211,13 +207,8 @@ void setup()
 {
 	setup_logging(devnum);
 	host_buffer = get_mapping(devnum, &fd);
-<<<<<<< HEAD
-    // Get the ao_buffer from the get_mapping function.
-    ao_only_buffer = get_mapping(ao_devnum, &ao_fd);
-=======
 	// Get the ao_buffer from the get_mapping function.
 	ao_only_buffer = get_mapping(ao_devnum, &ao_fd);
->>>>>>> 5a95de85c4eeb21c4a2f4b7796d9ea5d23f4b382
 	goRealTime();
 
 	xllc_def.len = samples_buffer*VI_LEN;
@@ -247,14 +238,6 @@ void setup()
 	}
 	printf("AO buf pa: 0x%08x len %d\n", xllc_def.pa, xllc_def.len);
 
-<<<<<<< HEAD
-    // Use ioctl to communicate AO LLC to AHBA404
-    if (ioctl(ao_fd, AFHBA_START_AO_LLC, &ao_only_xllc_def)){
-        perror("ioctl AFHBA_START_AO_LLC for AO only system.");
-        exit(1);
-    }
-    printf("AO buf pa: 0x%08x len %d\n", ao_only_xllc_def.pa, ao_only_xllc_def.len);
-=======
 	xllc_def.len = VO_ONLY_LEN;
 	// Use ioctl to communicate AO LLC to AHBA404
 	if (ioctl(ao_fd, AFHBA_START_AO_LLC, &xllc_def)){
@@ -262,7 +245,6 @@ void setup()
 		exit(1);
 	}
 	printf("AO buf pa: 0x%08x len %d\n", xllc_def.pa, xllc_def.len);
->>>>>>> 5a95de85c4eeb21c4a2f4b7796d9ea5d23f4b382
 
 	ao_buffer = (short*)((void*)host_buffer+AO_OFFSET);
 
@@ -376,12 +358,6 @@ void run(void (*control)(short *ao, short *ai), void (*action)(void*))
 		TLATCH(ai_buffer)[2] = pollcat;
 		TLATCH(ai_buffer)[3] = difftime_us();
 		action(ai_buffer);
-<<<<<<< HEAD
-        // Copy first 4 channels from ao_buffer into the ao_only_buffer.
-        memcpy(ao_only_buffer, ao_buffer, VO_ONLY_LEN);
-=======
-		
->>>>>>> 5a95de85c4eeb21c4a2f4b7796d9ea5d23f4b382
 		if (verbose){
 			print_sample(sample, tl1);
 		}
