2060b33d929050a4dbb00ebea9116a2c4fc5d9ee
peter.milne@d-tacq.co.uk <peter.milne@d-tacq.co.uk>
2020-09-10 14:36:48 +0100

    compilation fix 	modified:   STREAM/InlineDataHandlerMuxAO_STREAM_ALL.cpp

--- STREAM/InlineDataHandlerMuxAO_STREAM_ALL.cpp
+++ STREAM/InlineDataHandlerMuxAO_STREAM_ALL.cpp
@@ -75,10 +75,12 @@ class InlineDataHanderMuxAO_STREAM : public InlineDataHandler {
 	{
 		FILE *fp = fopen("/dev/shm/amx_ao_map", "r");
 		if (fp){
-			int _ao_ch, _ai_ch;
-			for (ao_ch = 0; ao_ch < ao_count; ++ao_ch){
+			for (int ao_ch = 0; ao_ch < ao_count; ++ao_ch){
 				ao_ai_mapping[_ao_ch] = AO_MAP_ZERO;
 			}
+
+			int _ao_ch, _ai_ch;
+
 			if (fscanf(fp, "%d,%d", &_ao_ch, &_ai_ch) == 2 &&
 					_ao_ch >= 1 && _ao_ch <= ao_count &&
 					_ai_ch >= 1 &&  _ai_ch <= ai_count){
