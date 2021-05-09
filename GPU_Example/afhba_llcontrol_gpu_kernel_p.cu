828209f17f533b0d7c1661175e01645820eef1a2
Peter Milne <peter.milne@d-tacq.com>
2021-05-09 11:54:27 +0100

    stop on missing tlatch. 	modified:   GPU_Example/afhba_llcontrol_gpu_kernel_p.cu

--- GPU_Example/afhba_llcontrol_gpu_kernel_p.cu
+++ GPU_Example/afhba_llcontrol_gpu_kernel_p.cu
@@ -4,13 +4,14 @@
 
 #define NSEC_PER_CLK  1		// SWAG
 
+__device__ int stop;
 
 __device__ void nsleep(unsigned nsec) {
 	long long start_clock = clock64();
 	long long clock_count = nsec/NSEC_PER_CLK;
 	//	printf("nsleep: nsec:%u clock_count:%llu\n", nsec, clock_count);
 	for (unsigned pollcat = 0; clock64() - start_clock  < clock_count; ) {
-		if (++pollcat&0xffff == 0){
+		if (++pollcat&0xfffff == 0){
 			printf("nsleep pc:%u start:%llu now:%llu end:%llu\n", pollcat, start_clock, clock64() - start_clock,  clock_count);
 		}
 	}
@@ -19,14 +20,20 @@ __device__ void nsleep(unsigned nsec) {
 __device__ int wait_sample(int ii, unsigned* tlp, unsigned tl0, short* pai0)
 {
 	unsigned tl;
+	unsigned tl0p1 = tl0+1;
+	
 	for (unsigned pollcat = 0; (tl = *tlp) == tl0; ){
-		if ((++pollcat&0x0fff) == 0){
+		if ((++pollcat&0x0fffff) == 0){
 			printf("ii:%10d pollcat:%08x nothing to see at %p %08x %04x %04x %04x %04x\n",
 					ii, pollcat, tlp, *tlp, pai0[0]&0xffff, pai0[1]&0xffff, pai0[2]&0xffff, pai0[3]&0xffff );
 		}else{
 			nsleep(1000);
 		}
-	}	
+	}
+	if (tl0p1 != tl){
+		printf("ERROR: wait_sample() %d missing tl tl0:%u wanted:%u got:%u %s\n", ii, tl0, tl0p1, tl, tl0p1 == tl? "EQ": "NE");
+		stop = 1;
+	}
 	return tl;
 }
 
@@ -48,7 +55,7 @@ __global__ void llcontrol_gpu_Amatrix(void * volatile ai_buffer_ptr,
 	unsigned tl0 = *tlatch;
 	unsigned tl;
 
-	for (int ii = 0; ii < nCycles; ii++) {
+	for (int ii = 0; !stop && ii < nCycles; ii++) {
 		if (proc0){
 			tl = wait_sample(ii, tlatch, tl0, pai0);
 		}
@@ -65,7 +72,7 @@ __global__ void llcontrol_gpu_Amatrix(void * volatile ai_buffer_ptr,
 			ao_result = -0x7fff;
 		}
 		pao0[ao] = (short)ao_result;
-#if 1      
+#if 0     
 		if (proc0 && ii%40000 == 0){
 			printf("Cycle: %10d tl:%10u tl0 %10u\n", ii, tl, tl0);
 			for (int iw = 0; iw < 80; ++iw){
