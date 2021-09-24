4d76d166d0e438be328e1cfd6c258b19232ddcc1
Peter Milne <peter.milne@d-tacq.com>
2021-09-24 09:37:05 +0100

    NetworkSystemInterface: example interface where AI -> "network",   "network" -> AO creates new executable acqproc_network
    This is a dummy implementation using stdin, stdout to simulate a network
    link.
    
    ** BEWARE: large binary output to stdout by default.
    ** BEWARE: non-blocking input on stdin not done.
    
    To extend:
    - for regular sockets, connect the socket, create a file handle, set the
      file handles..
    - for custom packets, the user will probably replace fread(), fwrite()
      with memcpy()
     On branch master
    	modified:   Makefile
    	new file:   NetworkSystemInterface.cpp

---
+++ ACQPROC/NetworkSystemInterface.cpp
@@ -0,0 +1,60 @@
+/*
+ * NetworkSystemInterface.cpp
+ *
+ *  Created on: 24 Sep 2021
+ *      Author: pgm
+ */
+
+
+
+
+#include "AcqSys.h"
+#include <string.h>
+
+
+class NetworkSystemInterface: public SystemInterface {
+
+	FILE* fp_out; 	// this could be a network device, default is stdout */
+	FILE* fp_in;    // this could be a non-blocking network device, default is stdin */
+
+	// Output to stdout in real life will be BINARY. hexdump required */
+	// Non-blocking input on stdin ... @todo.
+#define A_PACKET_IS_WAITING 0
+
+protected:
+	// a concrete subclass could override with useful implementations ..
+	virtual void sendPacket()
+	{
+		fwrite(IN.AI16, AI16_count(), sizeof(short), fp_out);
+		fwrite(IN.SP32, 4, sizeof(long), fp_out);
+	}
+	virtual void receivePacket()
+	{
+		if (A_PACKET_IS_WAITING){
+			fread(OUT.AO16, AO16_count(), sizeof(short), fp_in);
+		}
+	}
+public:
+	NetworkSystemInterface(const HBA& hba) :
+		SystemInterface(hba),
+		fp_out(stdout),
+		fp_in(stdin)
+	{
+		G::verbose && printf("%s::%s\n", __FILE__, PFN);
+	}
+
+
+	virtual void ringDoorbell(int sample){
+		G::verbose && printf("%s(%d)\n", PFN, sample);
+
+		sendPacket();
+		receivePacket();
+	}
+};
+
+SystemInterface& SystemInterface::factory(const HBA& hba)
+{
+	if (G::verbose) printf("%s::%s\n", __FILE__, PFN);
+
+	return * new NetworkSystemInterface(hba);
+}
