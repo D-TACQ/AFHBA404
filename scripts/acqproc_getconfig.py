4fdd038ed819803d53595f44174c25cd8f886621
sambelltacq <Sam.Bell@d-tacq.co.uk>
2023-09-26 12:22:59 +0100

    now handles sync role in json correctly 	modified:   scripts/acqproc_getconfig.py
    	modified:   scripts/acqproc_multi.sh
    
    	modified:   scripts/list_capture_uuts.py

--- scripts/acqproc_getconfig.py
+++ scripts/acqproc_getconfig.py
@@ -1,52 +1,58 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Script parses ACQPROC config file to create environment variables UUT1 [UUT2...] UUTS
 
-Usage:
-
+Usage: ./scripts/acqproc_getconfig.py llc_configs/config1.json
 
 """
 
-
 import argparse
-from os.path import expanduser
-import os
 import json
 
-
-def parse_config(path):
-    print("# parse_config {}".format(path))
-    with open(path) as f:
+def parse_config(args):
+    with open(args.config) as f:
         jdata = json.load(f)
 
-    uuts=''
-    sync_roles=''
-    ii = 1
+    uuts = []
+    sync_roles = []
+    imax = 1
+    has_master = False
 
     for uut in jdata['AFHBA']['UUT']:
+
         name = uut['name']
 
         if 'sync_role' in uut:
-            sync_roles += uut['sync_role'] + " "
+            sync_role = uut['sync_role']
         else:
-            sync_roles += "master " if ii==1 else "slave "
+            sync_role = 'slave' if has_master else 'master'
 
-        print("UUT{}={}".format(ii, uut['name']))
-        uuts = '{}{} '.format(uuts, uut['name'])
-        ii += 1
+        if 'master' in sync_role:
+            uuts.insert(0, name)
+            sync_roles.insert(0, sync_role)
+            has_master = True
+        else:
+            uuts.append(name)
+            sync_roles.append(sync_role)
 
-    print('SYNC_ROLES="{}"'.format(sync_roles))
-    print('UUTS="{}"'.format(uuts))
-    print('DEVMAX={}'.format(ii))
+        imax += 1
 
+    with open(args.env, "w") as f:
+        f.write(f"# parse_config {args.config}\n")
+        for ii, name in enumerate(uuts):
+            f.write(f'UUT{ii+1}={name}\n')
+        uuts = " ".join(uuts)
+        sync_roles = " ".join(sync_roles)
+        f.write(f'SYNC_ROLES="{sync_roles}"\n')
+        f.write(f'UUTS="{uuts}"\n')
+        f.write(f'DEVMAX={imax}\n')
 
 def run_main():
     parser = argparse.ArgumentParser(description='acqproc_getconfig')
     parser.add_argument('config', help="path to config file")
-    args = parser.parse_args();
-    parse_config(args.config)
-
+    parser.add_argument('--env', default='acqproc_multi.env', help="path to env file")
+    parse_config(parser.parse_args())
 
 if __name__ == '__main__':
     run_main()
