015977b718665266309f926b6fd7f75230b5de65
Peter Milne <peter.milne@d-tacq.com>
2024-09-26 20:37:18 +0100

    zack-vii : Remove usage of exit in RTM_T_Device; cleanup class and rtm-t-stream 	modified:   STREAM_LIB/RTM_T_Device.cpp 	modified:   STREAM_LIB/RTM_T_Device.h 	modified:   STREAM_LIB/rtm-t-stream.cpp

--- STREAM_LIB/RTM_T_Device.cpp
+++ STREAM_LIB/RTM_T_Device.cpp
@@ -9,109 +9,121 @@
  *  @brief interface to RTM_T Device (historic name for AFHBA404)
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
+#include <cerrno>
+#include <cstring>
+#include <fcntl.h>
+#include <stdexcept>
 #include <stdlib.h>
 #include <sys/mman.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
-#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include "local.h"
 #include "RTM_T_Device.h"
-
-
-
+#include "local.h"
 
 static unsigned calc_nbuffers(unsigned devnum);
 static unsigned calc_maxlen(unsigned devnum);
 static unsigned calc_transfer_buffers(unsigned devnum);
 
-RTM_T_Device::RTM_T_Device(int _devnum) :
-	devnum(_devnum%100), nbuffers(calc_nbuffers(devnum)),
-	maxlen(calc_maxlen(devnum)),
-	transfer_buffers(calc_transfer_buffers(devnum))
-{
-	char buf[120];
-
-	sprintf(buf, "/dev/rtm-t.%d", devnum);
-	names[MINOR_DMAREAD] = buf;
-	_open(MINOR_DMAREAD);
-
-	sprintf(buf, "/dev/rtm-t.%d.regs", devnum);
-	names[MINOR_REGREAD] = buf;
-	_open(MINOR_REGREAD);
-
-	for (int ib = 0; ib < nbuffers; ++ib){
-		sprintf(buf, "/dev/rtm-t.%d.data/hb%02d", devnum, ib);
-		names[ib] = buf;
-
-		_open(ib);
-		void *va = mmap(0, maxlen, PROT_READ|PROT_WRITE,
-				MAP_SHARED, handles[ib], 0);
-
-		if (va == (caddr_t)-1 ){
-			perror( "mmap" );
-		        _exit(errno);
-		}else{
-			host_buffers[ib] = va;
-		}
-	}
-
-	sprintf(buf, "/dev/rtm-t.%d.ctrl", devnum);
-	names[CTRL_ROOT] = buf;
+void throw_os_error(const std::string &hint, int err) {
+  errno = err;
+  throw std::runtime_error(hint);
 }
 
-static int getKnob(const char* knob, unsigned* value)
-{
-        FILE *fp = fopen(knob, "r");
-	if (!fp)
-	{
-		perror(knob);
-		return -1;
-	}
-        int rc = fscanf(fp, "%u", value);
-        fclose(fp);
-        return rc;
+int RTM_T_Device::_open(const char *name, int mode) {
+  const int fp = open(name, mode);
+  if (fp == -1) {
+    const int err = errno;
+    _close();
+    throw_os_error(std::string("open ") + name, err);
+  }
+  return fp;
 }
 
-#define PARAMETERS              "/sys/module/afhba/parameters/"
-#define BUFFER_LEN              PARAMETERS "buffer_len"
-#define NBUFFERS                PARAMETERS "nbuffers"
-#define TRANSFER_BUFFERS        PARAMETERS "transfer_buffers"
-
-static unsigned calc_transfer_buffers(unsigned devnum)
-{
-	unsigned transfer_buffers = 0;
-	if (getKnob(TRANSFER_BUFFERS, &transfer_buffers) != 1){
-		perror("getKnob " TRANSFER_BUFFERS " failed");
-		exit(1);
-	}
-	return transfer_buffers;
+inline void RTM_T_Device::_close() {
+  for (const auto buffer : host_buffers) {
+    munmap(buffer, maxlen);
+  }
+  if (dmaread_fd != -1)
+    close(dmaread_fd);
+  if (regread_fd != -1)
+    close(regread_fd);
 }
 
-static unsigned calc_nbuffers(unsigned devnum)
-{
-	unsigned nbuffers = 0;
-	if (getKnob(NBUFFERS, &nbuffers) != 1){
-		perror("getKnob " NBUFFERS " failed");
-		exit(1);
-	}
-	return nbuffers;
+RTM_T_Device::~RTM_T_Device() { _close(); }
+RTM_T_Device::RTM_T_Device(int _devnum)
+    : devnum(_devnum % 100), nbuffers(calc_nbuffers(devnum)),
+      maxlen(calc_maxlen(devnum)),
+      transfer_buffers(calc_transfer_buffers(devnum)) {
+  char name[128];
+
+  sprintf(name, "/dev/rtm-t.%u.ctrl", devnum);
+  name_ctlroot = name;
+
+  sprintf(name, "/dev/rtm-t.%u", devnum);
+  name_dmaread = name;
+  dmaread_fd = _open(name);
+
+  sprintf(name, "/dev/rtm-t.%u.regs", devnum);
+  name_regread = name;
+  regread_fd = _open(name);
+  host_buffers.reserve(nbuffers);
+  for (int ib = 0; ib < nbuffers; ++ib) {
+    sprintf(name, "/dev/rtm-t.%u.data/hb%04d", devnum, ib);
+    const int fd = _open(name);
+    void *const va = mmap(0, maxlen, PROT_READ, MAP_SHARED, fd, 0);
+    close(fd);
+    if (va == (void *)-1) {
+      const int err = errno;
+      _close();
+      throw_os_error(std::string("mmap ") + name, err);
+    } else {
+      host_buffers.push_back(va);
+    }
+  }
 }
-static unsigned calc_maxlen(unsigned devnum)
-{
-        char knob[80];
-	unsigned maxlen;
-	snprintf(knob, 80, "/dev/rtm-t.%u.ctrl/buffer_len", devnum);
-	if (getKnob(knob, &maxlen) == 1){
-		return maxlen;
-	}else if (getKnob(BUFFER_LEN, &maxlen) == 1){
-		return maxlen;
-        }else{
-		perror("maxlen not set");
-		exit(1);
-	}
+
+static int getKnob(const char *knob, unsigned *value) {
+  FILE *const fp = fopen(knob, "r");
+  if (!fp) {
+    return -1;
+  }
+  const int rc = fscanf(fp, "%u", value);
+  fclose(fp);
+  return rc;
+}
+
+#define PARAMETERS "/sys/module/afhba/parameters/"
+#define BUFFER_LEN PARAMETERS "buffer_len"
+#define NBUFFERS PARAMETERS "nbuffers"
+#define TRANSFER_BUFFERS PARAMETERS "transfer_buffers"
+
+static unsigned calc_transfer_buffers(unsigned devnum) {
+  unsigned transfer_buffers = 0;
+  if (getKnob(TRANSFER_BUFFERS, &transfer_buffers) != 1) {
+    throw std::runtime_error("getKnob " TRANSFER_BUFFERS " failed");
+  }
+  return transfer_buffers;
+}
+
+static unsigned calc_nbuffers(unsigned devnum) {
+  unsigned nbuffers = 0;
+  if (getKnob(NBUFFERS, &nbuffers) != 1) {
+    throw std::runtime_error("getKnob " NBUFFERS " failed");
+  }
+  return nbuffers;
+}
+static unsigned calc_maxlen(unsigned devnum) {
+  unsigned buffer_len = 0;
+  char knob[80];
+  snprintf(knob, 80, "/dev/rtm-t.%u.ctrl/buffer_len", devnum);
+  if (getKnob(knob, &buffer_len) == 1) {
+    return buffer_len;
+  } else if (getKnob(BUFFER_LEN, &buffer_len) == 1) {
+    return buffer_len;
+  } else {
+    throw std::runtime_error("buffer_len not set");
+  }
 }
