8f60136f95f70c643b7873efd66f7596f44a4e51
nadirbstfc <97950232+nadirbstfc@users.noreply.github.com>
2026-06-09 11:51:24 +0100

    fix STREAM_LIB streaming permission (#136)

--- STREAM_LIB/RTM_T_Device.cpp
+++ STREAM_LIB/RTM_T_Device.cpp
@@ -64,7 +64,7 @@ RTM_T_Device::RTM_T_Device(int _devnum)
 
   sprintf(name, "/dev/rtm-t.%u", devnum);
   name_dmaread = name;
-  dmaread_fd = _open(name);
+  dmaread_fd = _open(name, O_RDWR); // release_buffer write()s to this fd; O_RDONLY silently fails
 
   sprintf(name, "/dev/rtm-t.%u.regs", devnum);
   name_regread = name;
